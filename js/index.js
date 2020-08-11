54eeff975c8ce8324762472b9a7a260ea03ee2a8
Arpit Jaiswal <arpitjaiswal282000@gmail.com>
2020-08-12 00:49:00 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,75 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'li') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function todo() {
+
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("text").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    }
+    
+    else {
+      document.getElementById("MyUl").appendChild(li);
+    }
+    document.getElementById("text").value = " ";
+  
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  }
+  document.getElementsByTagName("button")
+        .addEventListener("click", function(){
+            return(
+                todo();
+            );
+        })
+
+
+
+
+
+
+
+
+
+    
+
+    
